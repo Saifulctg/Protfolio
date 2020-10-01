364d94a329a5e03b11e407eb9e0641f140684f90
Saiful Islam <67482122+Saifulctg@users.noreply.github.com>
2020-10-02 01:56:15 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,4 +44,91 @@ $(function () {
    })
    // Header Fixed Code End
 
+   // Isotope Filter Code Start
+   var $grid = $('.protfolio_item').isotope();
+
+    $('.work_list').on( 'click', 'li', function() {
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
+    });
+   // Isotope Filter Code End
+
+   // Menu Active Code Start
+
+   $('.work_list li').on('click', function () {
+      $('.work_list li').removeClass('active_li')
+      $(this).addClass('active_li')
+   })
+   // Menu Active Code End
+
+   // Venobox Code Start
+   $('.venobox').venobox(); 
+   // Venobox Code End
+
+   // Minimal Progress Bar Code Start
+   $('#web').LineProgressbar({
+        percentage: 90,
+        fillBackgroundColor:'#3498db',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2200
+      });
+   $('#customize').LineProgressbar({
+        percentage: 95,
+        fillBackgroundColor:'#da916f',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2500
+      });
+   $('#seo').LineProgressbar({
+        percentage: 70,
+        fillBackgroundColor:'#54e2ac',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2000
+      });
+   $('#marketing').LineProgressbar({
+        percentage: 65,
+        fillBackgroundColor:'#a254e2',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2000
+      });
+   $('#html').LineProgressbar({
+        percentage: 90,
+        fillBackgroundColor:'#3498db',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2200
+      });
+   $('#css').LineProgressbar({
+        percentage: 95,
+        fillBackgroundColor:'#54e2ac',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2500
+      });
+   $('#java').LineProgressbar({
+        percentage: 70,
+        fillBackgroundColor:'#54a2e2',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2000
+      });
+   $('#bootstrap').LineProgressbar({
+        percentage: 65,
+        fillBackgroundColor:'#da916f',
+        backgroundColor:'#e1e1e1',
+        radius:'0 5px 5px 0',
+        duration: 2000
+      });
+   // Minimal Progress Bar Code End
+
+   // Counter Up Code Start
+   $('.counter').counterUp({
+      delay: 20,
+      time: 2000
+  });
+   // Counter Up Code End
+
 })
